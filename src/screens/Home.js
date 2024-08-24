184669b6eee8e1782e2a8486d300fdd63c7c00a6
NarutoUzumakioftheleaf <deepapandey717@gmail.com>
2024-08-24 12:28:33 +0530

    Add all project files

---
+++ src/screens/Home.js
@@ -0,0 +1,140 @@
+import React, { useEffect, useState } from "react";
+import Navbar from "../components/Navbar";
+import Card from "../components/Card";
+
+import Footer from "../components/Footer";
+export default function Home() {
+  const [search, setSearch] = useState("");
+  const [foodCat, setFoodCat] = useState([]);
+  const [foodItem, setFoodItem] = useState([]);
+  const loadData = async () => {
+    let response = await fetch("http://localhost:5000/api/foodData", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    response = await response.json();
+    setFoodItem(response[0]);
+    setFoodCat(response[1]);
+    // console.log(response[0],response[1]);
+  };
+  useEffect(() => {
+    loadData();
+  }, []);
+
+  return (
+    <div>
+      <div>
+        <Navbar />
+      </div>
+      <div>
+        <div
+          id="carouselExampleControls"
+          className="carousel slide"
+          data-bs-ride="carousel"
+          style={{ objectFit: "contain !important" }}>
+          <div className="carousel-inner" style={{ height: "500px" }}>
+            <div className="carousel-item active">
+              <img
+                src="https://media.istockphoto.com/id/520410807/photo/cheeseburger.jpg?s=612x612&w=0&k=20&c=fG_OrCzR5HkJGI8RXBk76NwxxTasMb1qpTVlEM0oyg4="
+                className="d-block w-100"
+                alt="..."
+              />
+            </div>
+            <div className="carousel-item">
+              <img
+                src="https://media.istockphoto.com/id/184946701/photo/pizza.jpg?s=612x612&w=0&k=20&c=97rc0VIi-s3mn4xe4xDy9S-XJ_Ohbn92XaEMaiID_eY="
+                className="d-block w-100"
+                alt="..."
+              />
+            </div>
+            <div className="carousel-item">
+              <img
+                src="https://media.istockphoto.com/id/1215178290/photo/stir-fry-noodles-with-vegetables-and-beef-in-black-bowl-slate-background-close-up.jpg?s=612x612&w=0&k=20&c=_svdOGLzJ52q1Mf_k6v5u5Xc5iRkH0DIYrwmrfKTaJ4="
+                className="d-block w-100"
+                alt="..."
+              />
+            </div>
+            <div className="carousel-caption">
+              <div className="form-inline d-flex justify-content-center">
+                <input
+                  className="form-control  mr-2"
+                  type="search"
+                  placeholder="Search"
+                  aria-label="Search"
+                  value={search}
+                  onChange={(e) => {
+                    setSearch(e.target.value);
+                  }}
+                  style={{ marginRight: "10px" }}
+                />
+              </div>
+            </div>
+          </div>
+
+          <button
+            className="carousel-control-prev"
+            type="button"
+            data-bs-target="#carouselExampleControls"
+            data-bs-slide="prev">
+            <span
+              className="carousel-control-prev-icon"
+              aria-hidden="true"></span>
+            <span className="visually-hidden">Previous</span>
+          </button>
+          <button
+            className="carousel-control-next"
+            type="button"
+            data-bs-target="#carouselExampleControls"
+            data-bs-slide="next">
+            <span
+              className="carousel-control-next-icon"
+              aria-hidden="true"></span>
+            <span className="visually-hidden">Next</span>
+          </button>
+        </div>
+      </div>
+      <div className="container">
+        {foodCat.length !== 0
+          ? foodCat.map((data) => {
+              return (
+                <div className="row mb-3">
+                  <div key={data._id} className="fs-3 m-3">
+                    {data.CategoryName}
+                  </div>
+                  <hr />
+                  {foodItem.length !== 0 ? (
+                    foodItem
+                      .filter(
+                        (item) =>
+                          item.CategoryName === data.CategoryName &&
+                          item.name
+                            .toLowerCase()
+                            .includes(search.toLocaleLowerCase())
+                      )
+                      .map((filterItems) => {
+                        return (
+                          <div
+                            key={filterItems._id}
+                            className="col-12 col-mid-6 col-lg-3">
+                            <Card
+                              foodItem={filterItems}
+                              options={filterItems.options[0]}></Card>
+                          </div>
+                        );
+                      })
+                  ) : (
+                    <div>Not Found</div>
+                  )}
+                </div>
+              );
+            })
+          : ""}
+      </div>
+      <div>
+        <Footer />
+      </div>
+    </div>
+  );
+}
